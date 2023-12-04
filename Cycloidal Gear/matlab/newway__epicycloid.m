35cd6db381cfd7fb519597cb8712041a70492cf4
boompeko <a1ropvjz@gmail.com>
2023-12-05 05:01:39 +0800

    1205

--- Cycloidal Gear/matlab/newway__epicycloid.m
+++ Cycloidal Gear/matlab/newway__epicycloid.m
@@ -20,11 +20,11 @@ E =5 ;% Eccentricity - offset from input shaft to a cycloidal disk
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-CUT = 40;%切割倍數
-tick = 20;
+CUT = 20;%切割倍數
+tick = 40;
 SHOW = sprintf("N = %d, Rr = %d, R = %d, E = %d", N, Rr, R, E);
-%file_path = 'C:\Users\JOU\Desktop\git\Special-project\Cycloidal Gear\output'; %家裡電腦
-file_path = 'C:\Users\Johnny Jou\Documents\GitHub\Special-project\Cycloidal Gear\output';  %筆記電腦
+file_path = 'C:\Users\JOU\Desktop\git\Special-project\Cycloidal Gear\output'; %家裡電腦
+%file_path = 'C:\Users\Johnny Jou\Documents\GitHub\Special-project\Cycloidal Gear\output';  %筆記電腦
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -61,19 +61,71 @@ end
 
 
 a = figure('Visible', 'on');
-plot(X,Y,'LineWidth',2);
-xlabel('X','fontname','Times New Roman','fontsize',20');
-ylabel('Y','fontname','Times New Roman','fontsize',20');
-title('擺線輪輪廓',SHOW);
+plot(X,Y,'LineWidth',2,'Color','b');
+xlabel('X','fontname','Times New Roman','fontsize',18');
+ylabel('Y','fontname','Times New Roman','fontsize',18');
+set(gca, 'Fontname', 'Times New Roman','FontSize',14);
+%title('擺線輪輪廓',SHOW);
 axis equal;
-xlim([-(R+2*Rr),(R+2*Rr)]);
-ylim([-(R+2*Rr),(R+2*Rr)]);
+xlim([-(R),(R)]);
+ylim([-(R),(R)]);
 xticks(-2*R:tick:2*R);
 yticks(-2*R:tick:2*R);
 
 box on;
 grid on;
 axis square;
+
+file_name = '外擺線輪.png';
+
+
+full_file_path = fullfile(file_path, file_name);
+
+saveas(gcf, full_file_path);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%繪製曲率半徑
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+z(1) = 0;
+bRc(1) = newRc(360*CUT);
+
+for i = 1 : 1 : ceil(360*CUT/(N-1))
+
+    z(i+1) = i/CUT ;
+    bRc(i+1) = newRc(i);
+
+end
+
+
+b = figure('Visible', 'on');
+plot(z,bRc,'LineWidth',2, 'Color','b');
+xlabel('cycloidal disk rotation angle (θ)','fontname','Times New Roman','fontsize',18');
+ylabel('radius of curvature (mm)','fontname','Times New Roman','fontsize',18');
+set(gca, 'Fontname', 'Times New Roman','FontSize',14);
+%title(SHOW,'曲率半徑','FontSize',16);
+xlim([0,ceil(360/(N-1))]);
+ylim([-R,R]);
+xticks(0:5:(360/(N-1)));
+yticks(-R:tick:R);
+
+box on;
+grid on;
+axis square;
+
+file_name = 'radius_of_curvature_epicycloid.png';
+
+
+full_file_path = fullfile(file_path, file_name);
+
+saveas(gcf, full_file_path);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%誤差分析
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 
 for i=1:1:ceil(360*CUT/(N-1))
@@ -91,11 +143,8 @@ for i=1:1:ceil(360*CUT/(N-1))
     
     I(i) = 2*a1*a3*cos((1-N)*t(i)) - 2*a2*a3;
 
-    Tup(i) = ((newRc(i)+Rr)*0.02);
-    Tdo(i) = (H(i)*cos(zeta)-I(i)*sin(zeta));
-    
-    T(i) = Tup(i)/Tdo(i);
-    
+    T(i) = ((newRc(i)+Rr)*0.02)/(H(i)*cos(zeta)-I(i)*sin(zeta));
+
     
     f(i) = i/CUT ;
 
@@ -103,14 +152,15 @@ end
 
 
 g = figure('Visible', 'on');
-plot(f,T,'LineWidth',2);
-xlabel('cycloidal disk rotation angle (θ)','fontname','Times New Roman','fontsize',20');
-ylabel('Errors (θ)','fontname','Times New Roman','fontsize',20');
-title(SHOW,'誤差分析','fontname','標楷體','FontSize',16);
+plot(f,T,'LineWidth',2,'Color','b');
+xlabel('cycloidal disk rotation angle (θ)','fontname','Times New Roman','fontsize',18');
+ylabel('Errors (θ)','fontname','Times New Roman','fontsize',18');
+set(gca, 'Fontname', 'Times New Roman','FontSize',14);
+%title(SHOW,'誤差分析','fontname','標楷體','FontSize',16);
 xlim([0,ceil(360/(N-1))]);
-ylim([-0.5/1000,0.5/1000]);
+%ylim([-0.5/1000,0.5/1000]);
 % xticks(0:5:(360/(N-1)));
-yticks(-0.5/1000:10:0.5/1000);
+%yticks(-0.5/1000:10:0.5/1000);
 
 box on;
 grid on;
